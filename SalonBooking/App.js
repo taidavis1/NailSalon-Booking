3e4324a0c84eb93bc20b77023b426ed769ee62d8
Levan Fuentes-Parra <levanfuentes-parra@Levans-MacBook-Air.local>
2023-09-19 01:24:16 -0700

    changes

--- SalonBooking/App.js
+++ SalonBooking/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import {Text, View } from 'react-native';
+import {Text, View , Image , TouchableOpacity} from 'react-native';
 import React from 'react';
 
 export default function App() {
   return (
-    <View className = "bg-black">
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className = "bg-slate-500 border-2 border-red-200 flex-1 px-12 py-3 space-y-24">
+      <View className = " border-2 mt-12 flex flex-row max-w-screen-lg">
+        <TouchableOpacity>
+          <Image className = "w-12 h-20 border-2 p-5 mt-12 rounded-full hover:opacity-50" source = {require('./src/img/Logo.jpeg')} />
+        </TouchableOpacity>
+        <View>
+          <Text>dadadad</Text>
+        </View>
+      </View>
+      <View className = "font-normal border-2 border-red-200 px-5 py-4">
+        <Text className = "text-2xl italic ">Welcome to Our Salon</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
