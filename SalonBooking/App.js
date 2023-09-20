d71989e9cbf09e2d815f2310fa854266230f9ee2
Levan Fuentes-Parra <levanfuentes-parra@Levans-MacBook-Air.local>
2023-09-20 00:57:05 -0700

    update lottie

--- SalonBooking/App.js
+++ SalonBooking/App.js
@@ -3,38 +3,44 @@ import { Button, StyleSheet, View, Text, SafeAreaView, Alert, TouchableOpacity }
 import LottieView from 'lottie-react-native';
 
 export default function App() {
-    const animation = useRef(null);
+    const animationRef = useRef(null);
     const [isLoading, setIsLoading] = useState(false);
     function toHomeScreen() {
         setIsLoading(!isLoading);
         console.log(isLoading);
     }
-    useEffect(() => { }, []);
-
+    useEffect(() => {
+        if (animationRef.current){
+            animationRef.current.play();
+        }
+    }, []);
     return (
-        <SafeAreaView>
+        <SafeAreaView className = "bg-slate-400/75 flex-1">
             {
                 !isLoading
                     ?
                     <View>
                         <View className="flex flex-row items-baseline max-h-screen ">
-                            <LottieView
-                                autoPlay
+                            <LottieView 
+                                ref={(animation) => {
+                                    animationRef.current = animation;
+                                }}
                                 loop
+                                autoPlay
                                 style={styles.centered}
                                 source={require('./assets/animations/animation_nails.json')}
                             />
                         </View>
                         <View className="flex flex-row justify-center mt-2">
                             <View className="flex flex-col justify-center mt-5 text-center items-center">
-                                <Text className="italic" style={styles.subtitle}>Tai Nails</Text>
-                                <Text style={styles.title}>Welcome to Our Salon</Text>
+                                <Text className="italic text-3xl" style={styles.subtitle}>Tai Nails</Text>
+                                <Text className = "text-2xl ml-4" style={styles.title}>Welcome to Our Salon</Text>
                                 <View>
                                     <TouchableOpacity
-                                        className="bg-[#fa6192] text-white text-center items-center w-[100px] h-[50px] mt-5 rounded"
+                                        className="bg-[#fa6192] items-center capitalize text-white text-center px-5 py-3 mt-5 rounded"
                                         onPress={() => toHomeScreen()}
                                     >
-                                        <Text className="mt-3 text-lg">Check In</Text>
+                                        <Text className="text-xl">Check In</Text>
                                     </TouchableOpacity>
                                 </View>
                             </View>
@@ -61,11 +67,9 @@ const styles = StyleSheet.create({
         alignItems: "center",
     },
     title: {
-        fontSize: 18,
         marginVertical: 2,
     },
     subtitle: {
-        fontSize: 16,
         color: "#888",
     },
 });
